1fc60bea85361663fa7e137b68cd30f5379fd67f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 13:39:54 -0500

    zip file 4 github

--- dev/upload.bat
+++ dev/upload.bat
@@ -2,8 +2,12 @@
 
 echo HTML5
 lime build html5 --times
+
+wsl zip -r export/html5.zip export/release/html5/bin/
 butler push ./export/release/html5/bin bopel-maki-macohi/simple-click:html5 --userversion-file version.txt
 
 echo WINDOWS
 lime build windows --times
+
+wsl zip -r export/windows.zip export/release/windows/bin/
 butler push ./export/release/windows/bin bopel-maki-macohi/simple-click:windows --userversion-file version.txt
