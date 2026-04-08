fc4cdcf1cae0a2d1e1d05f7e729ee86fc3255c67
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 13:13:36 -0500

    IM STUPID LMAO

--- dev/upload-web.bat
+++ dev/upload-web.bat
@@ -2,4 +2,4 @@
 echo BUILDING HTML5
 lime build html5
 echo UPLOADING TO ITCH
-butler push ./export/release/html5/bin https://bopel-maki-macohi.itch.io/simple-click:html5
+butler push ./export/release/html5/bin bopel-maki-macohi.itch.io/simple-click:html5
