33079763e0b1040d6e844ed19b90783786805423
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 13:27:44 -0500

    version text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,19 @@
 package;
 
+import flixel.FlxG;
+import flixel.text.FlxText;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var _versionText:FlxText;
+
 	override public function create()
 	{
 		super.create();
+
+		_versionText = new FlxText(0, 0, 0, FlxG.stage.application.meta.get('version'), 16);
+		add(_versionText);
 	}
 
 	override public function update(elapsed:Float)
