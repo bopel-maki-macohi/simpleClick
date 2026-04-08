5248edd8c5b1ccd84bf36f29e59593a6a4f8c204
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 13:20:01 -0500

    version file

--- dev/upload.bat
+++ dev/upload.bat
@@ -1,13 +1,9 @@
 @echo off
 
-echo BUILDING HTML5
+echo HTML5
 lime build html5 --times
+butler push ./export/release/html5/bin bopel-maki-macohi/simple-click:html5 --userversion-file version.txt
 
-echo BUILDING WINDOWS
+echo WINDOWS
 lime build windows --times
-
-
-echo UPLOADING TO ITCH
-
-butler push ./export/release/html5/bin bopel-maki-macohi/simple-click:html5
-butler push ./export/release/windows/bin bopel-maki-macohi/simple-click:windows
+butler push ./export/release/windows/bin bopel-maki-macohi/simple-click:windows --userversion-file version.txt
