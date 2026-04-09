1437bc561e60d5a3dbf805798016c85bae5e12f5
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 19:24:21 -0500

    changed upload.bat

--- upload.bat
+++ upload.bat
@@ -1,11 +1,14 @@
 @echo off
 
-echo HTML5
-lime build html5 --times
-butler push ./export/release/html5/bin bopel-maki-macohi/simple-click:html5 --userversion-file version.txt
+echo BUILDING: HTML5
+lime build html5 -clean --times
 
-echo WINDOWS
-lime build windows --times
+echo BUILDING: WINDOWS
+lime build windows -clean --times
+
+echo UPLOADING: HTML5
+butler push ./export/release/html5/bin bopel-maki-macohi/simple-click:html5 --userversion-file version.txt
+echo UPLOADING: WINDOWS
 butler push ./export/release/windows/bin bopel-maki-macohi/simple-click:windows --userversion-file version.txt
 
 .\dev/zip.bat
