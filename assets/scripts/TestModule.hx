f771663d2732cb5d975b742272021358419eff4b
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 22:21:45 -0500

    figuring out scripted class shit

---
+++ assets/scripts/TestModule.hx
@@ -0,0 +1,11 @@
+package mods.scriptingTestMod.scripts;
+
+import scripting.Module;
+
+class TestModule extends Module
+{
+	override public function new()
+	{
+		super('test');
+	}
+}
---
+++ mods/scriptingTestMod/scripts/TestDosModule.hx
@@ -0,0 +1,11 @@
+package mods.scriptingTestMod.scripts;
+
+import scripting.Module;
+
+class TestDosModule extends Module
+{
+	override public function new()
+	{
+		super('test2');
+	}
+}
