602f5bf36191f7784dd1497c05c0b14a7256cf26
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 19:24:51 -0500

    zipping echos

--- dev/zip.bat
+++ dev/zip.bat
@@ -1,7 +1,11 @@
+@echo off
+
+echo ZIPPING: HTML5
 cd "export/release/html5/bin/"
 wsl zip -r "html5.zip" *
 wsl mv "html5.zip" ../../../../export/
 
+echo ZIPPING: WINDOWS
 cd "../../../../export/release/windows/bin/"
 wsl zip -r "windows.zip" *
 wsl mv "windows.zip" ../../../../export/
